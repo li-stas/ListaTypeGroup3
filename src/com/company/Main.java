5d609332dacadc3083af1f7b09dae55de7cfc50e
lista <lista@bk.ru>
2019-10-09 23:28:02 +0300

    ASort() ASort1()

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,11 +2,19 @@ package com.company;
 
 import com.company.arrays.Array;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
         int aM[] = {1,2,3,4,5};
+        String objStr = "";
         Array obj = new Array( aM );
-        System.out.println( obj.findMaxElem());
+        //System.out.println( obj.findMaxElem());
+        objStr = Arrays.toString(obj.getArray());
+        System.out.println( objStr );
+        obj.ASort1();
+        objStr = Arrays.toString(obj.getArray());
+        System.out.println( objStr );
     }
 }
--- src/com/company/arrays/Array.java
+++ src/com/company/arrays/Array.java
@@ -27,4 +27,30 @@ public class Array {
         }
         return max;
     }
+    public void ASort() {
+        int nElem;
+        for (int j = 1; j <= array.length ; j++) {
+            for (int i = 1; i <= array.length - 1 ; i++) {
+                if ( array[ (i - 1) ] < array[ (i - 1) + 1 ]) {
+                    // меняем местами
+                    nElem = array[ (i - 1) ];
+                    array[ (i - 1) ] = array[ (i - 1) + 1 ];
+                    array[ (i - 1) + 1 ] = nElem;
+                }//endif
+            }//next i
+        }//next j
+     }
+    public void ASort1() {
+        int nElem;
+        for (int j = 1; j <= array.length ; j++) {
+            for (int i = 0; i < (array.length - 1) ; i++) {
+                if ( array[ i ] < array[ i + 1 ] ) {
+                    // меняем местами
+                    nElem = array[ i ];
+                    array[ i ] = array[ i + 1 ];
+                    array[ i + 1 ] = nElem;
+                }//endif
+            }//next i
+        }//next j
+    }
 }
